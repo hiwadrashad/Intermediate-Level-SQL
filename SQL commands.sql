af10b35e32ac59dae89f4c4740db15826f589bdc
hiwad rashad <hiwadrashad112@hotmail.com>
2022-03-30 16:52:33 +0200

    Added Joins

--- SQL commands.sql
+++ SQL commands.sql
@@ -69,3 +69,55 @@ ORDER BY p.person_last_name;
 SELECT COUNT(p.person_first_name)
 FROM person p;
 
+/* Parenthesese can be used as a requirement for different command's 
+and it can be used to apply a flow where the command applied 
+to the statement will be executed first */
+
+/* GROUP BY used with the select query, if applied it will create sets grouped by the given parameter, 
+if not the count will be applied to the cumulative number of the given parameter applied to the first column name
+*/
+
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country;
+
+/* HAVING is like where =*/
+
+SELECT 
+COUNT(p.person_first_name),
+p.person_first_name
+GROUP BY p.person_first_name
+HAVING COUNT(p.person_first_name) > 0;
+
+/*CROSS join uses no keyword and no WHERE clause, creates cartesian product of selected tables and row (all possible combinations)*/
+
+USE contacts;
+
+SELECT 
+p.person_first_name,
+p.person_last_name,
+e.email_address
+FROM
+person p,
+email_adress e
+
+/*INNER JOIN joins two tables, doesn't deal with null values, where ON will join where the given requirements are met, this clause is optional */
+
+USE contacts;
+
+SELECT 
+p.person_first_name,
+p.person_last_name,
+e.email_address
+FROM
+person p
+INNER JOIN
+email_adress e
+ON
+p.person_id = e.email_address_person_id;
+
+/*FULL OUTER JOIN combines the two tables given*/
+
+/*LEFT OUTER JOIN gives all of the values fromt he left table and all of the matching values from the right and left table*/
+
+
